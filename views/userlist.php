324aa6323509605a3d0409110910e07dd0d52173
adithep55 <adithep.p@mutsmail.com>
2024-10-17 17:41:08 +0700

    ๊favicon

--- views/userlist.php
+++ views/userlist.php
@@ -7,12 +7,8 @@ requirePermission(['manage_users']);
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-    <meta name="description" content="POS - Bootstrap Admin Template">
-    <meta name="keywords" content="admin, estimates, bootstrap, business, corporate, creative, invoice, html5, responsive, Projects">
-    <meta name="author" content="Dreamguys - Bootstrap Admin Template">
-    <meta name="robots" content="noindex, nofollow">
     <title>รายการผู้ใช้</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/logo-small.png">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/animate.css">
     <link rel="stylesheet" href="../assets/plugins/select2/css/select2.min.css">
@@ -20,6 +16,7 @@ requirePermission(['manage_users']);
     <link rel="stylesheet" href="../assets/plugins/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="../assets/plugins/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body>
